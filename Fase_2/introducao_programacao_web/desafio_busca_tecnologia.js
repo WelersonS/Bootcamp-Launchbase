34164443be3f4bfe2a318e8192f139e5cd46d902
welerson58@gmail.com <welerson58@gmail.com>
2021-03-05 21:51:32 -0300

    desafios03

---
+++ Fase_2/introducao_programacao_web/desafio_busca_tecnologia.js
@@ -0,0 +1,19 @@
+const usuarios = [
+  { nome: "Carlos", tecnologias: ["HTML", "CSS"] },
+  { nome: "Jasmine", tecnologias: ["JavaScript", "CSS"] },
+  { nome: "Tuane", tecnologias: ["HTML", "Node.js"] }
+]
+
+function checaSeUsuarioUsaCSS(usuario) {
+  if(usuario.tecnologias.includes('CSS')) {
+    return true
+  }
+}
+
+for (let i = 0; i < usuarios.length; i++) {
+  const usuarioTrabalhaComCSS = checaSeUsuarioUsaCSS(usuarios[i]);
+
+  if (usuarioTrabalhaComCSS) {
+    console.log(`O usuário ${usuarios[i].nome} trabalha com CSS`);
+  }
+}
