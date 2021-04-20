c3497c89227b7999a85cffb0f891e00bea457fea
welersons <welerson58@gmail.com>
2021-04-20 18:11:03 -0300

    create file script

---
+++ Fase_2/iniciando_front_end/advertise_company/scripts.js
@@ -0,0 +1,14 @@
+const modalOverlay = document.querySelector('.modal-overlay')
+const cards = document.querySelectorAll('.cards')
+
+for (let card of cards) {
+    card.addEventListener('click', function() {
+        const postId = card.getAttribute('id')
+        modalOverlay.classList.add('active')
+        modalOverlay.querySelector('iframe').src = `https://blog.rocketseat.com.br/${postId}`
+    })
+}
+
+document.querySelector('.close-modal').addEventListener('click', function() {
+    modalOverlay.classList.remove('active')
+})
