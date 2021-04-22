af1618f1bf87e323c83aa2a449bffca744457dfb
welersons <welerson58@gmail.com>
2021-04-22 11:30:28 -0300

    add features
    feature maximize/minimize modal
    dynamic post adress

--- Fase_2/iniciando_front_end/advertise_company/scripts.js
+++ Fase_2/iniciando_front_end/advertise_company/scripts.js
@@ -1,14 +1,29 @@
 const modalOverlay = document.querySelector('.modal-overlay')
-const cards = document.querySelectorAll('.cards')
+const cards = document.querySelectorAll('.card')
+const modal = document.querySelector('.modal')
 
 for (let card of cards) {
     card.addEventListener('click', function() {
         const postId = card.getAttribute('id')
         modalOverlay.classList.add('active')
         modalOverlay.querySelector('iframe').src = `https://blog.rocketseat.com.br/${postId}`
+        modal.classList.remove('maximize')
     })
 }
 
 document.querySelector('.close-modal').addEventListener('click', function() {
     modalOverlay.classList.remove('active')
+    modalOverlay.querySelector('iframe').src = ''
+})
+
+document.querySelector('.maximize-modal').addEventListener('click', function() {
+    if (!modal.classList.contains('maximize')) {
+        modal.classList.add('maximize')
+    }
+})
+
+document.querySelector('.minimize-modal').addEventListener('click', function() {
+    if (modal.classList.contains('maximize')) {
+        modal.classList.remove('maximize')
+    }
 })
